995164ba29cc09ddfad4b5b26aee80c83cce36f8
chiba <qwq@qwwq.org>
2026-03-06 06:30:07 +0700

    v1.0(a) 优化了代码

---
+++ src/components/ts/useYamlI18n.ts
@@ -0,0 +1,34 @@
+import { computed, onMounted, ref } from "vue";
+import { lang, langMap } from "@/components/ts/useStoage";
+import { loadSingleYaml } from "@/components/ts/getYaml.ts";
+import yamlUrl from "../../../public/data/config/yamlUrl.json";
+
+interface IntroductionBlock {
+  type: string;
+  content: string;
+}
+
+interface Introduction {
+  blocks?: IntroductionBlock[];
+  content?: string;
+
+  [key: string]: unknown;
+}
+
+export const useYamlText = (type: keyof typeof yamlUrl, fileName: string) => {
+  const introData = ref<Introduction | null>(null);
+
+  onMounted(async () => {
+    introData.value = await loadSingleYaml<Introduction>(type, fileName);
+  });
+
+  return computed(() => {
+    if (!introData.value || !introData.value.blocks) return "Loading...";
+    const targetType = langMap[lang.value] || "en";
+    const { blocks } = introData.value;
+
+    return blocks.find((b) => b.type === targetType)?.content
+      || blocks.find((b) => b.type === "en")?.content
+      || "";
+  });
+};

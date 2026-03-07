2786e2462d95e0826317d543a11d7d50699314eb
chiba <qwq@qwwq.org>
2026-03-08 04:52:07 +0700

    v1.1.7 1.修改了test脚本以便更加兼容vue 2.修改了路由配置以避免用户输入非法地址，任何非法地址都会返回主页并弹窗

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
+import commonI18n from "@/data/I18N/commonI18n.json";
+import { lang } from "@/components/ts/useStorage";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -14,6 +16,22 @@ const router = createRouter({
       name: "blog",
       component: () => import("../views/BlogView.vue"),
       props: true,
+    },
+    {
+      path: "/:pathMatch(.*)*",
+      name: "NotFound",
+      component: HomeView,
+      beforeEnter: async (to, from, next) => {
+        const { $message } = await import("@/components/ts/msgUtils");
+        const i18nSource = commonI18n.invalidAccess as Record<string, string>;
+        const warningMsg = i18nSource[lang.value] || i18nSource["en"];
+        $message.warning(warningMsg, true, 4000);
+        if (from.name) {
+          next(false);
+        } else {
+          next({ name: "home" });
+        }
+      },
     },
   ],
 });
